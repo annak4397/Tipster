4423568cf6a7212880d13fe648b7d6d058776c8c
Anna Kuznetsova <annavk@fb.com>
2020-06-23 18:15:32 -0700

    Added the basic functions for the tip calculator

--- Tipster/ViewController.m
+++ Tipster/ViewController.m
@@ -10,6 +10,15 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+
+
 @end
 
 @implementation ViewController
@@ -19,5 +28,28 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)onTap:(id)sender {
+    NSLog(@"Hello");
+    [self.view endEditing:YES];
+}
+
+- (IBAction)onEdit:(id)sender {
+    
+    double bill = [self.billField.text doubleValue];
+    
+    NSArray *percentages = @[@(0.15), @(0.20), @(0.22)];
+    
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    
+    double tip = bill * tipPercentage;
+    double total = tip + bill;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f",tip] ;
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f",total];
+}
+- (IBAction)onEditingBegin:(id)sender {
+}
+- (IBAction)onEditingEnd:(id)sender {
+}
 
 @end
